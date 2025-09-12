77b922f97b630a563fe671abf2b54ac8d544cb27
klbops <klb.ep00@gmail.com>
2025-09-12 16:04:25 -0400

    super_trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,41 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  
+  char estado [50];
+  char codigo [50];
+  char cidade [50];
+  int populacao;
+  float area;
+  float pib;
+  int turisticos;
+
 
   // Área para entrada de dados
 
+  printf(" Digite o nome do estado: ");
+  scanf("%s", estado);
+
+  printf("Digite aqui o codigo: ");
+  scanf("%s", codigo);
+
+  printf("Digite o nome da cidade: ");
+  scanf("%s", cidade );
+
+  printf("Digite o numero da população: ");
+  scanf("%d", &populacao);
+
+  printf("Digite a area da cidade: ");
+  scanf("%f", &area);
+
+  printf("Digite o PIB: ");
+  scanf("%f", &pib);
+
+  printf("Digite os pontos turisticos: ");
+  scanf("%d", &turisticos);
+
   // Área para exibição dos dados da cidade
+  printf(" Nome do estado: %s - codigo da cidade: %s - nome da cidade: %s - Numero da populacao: %d - area da cidade: %f - digite o pib: %f - pontos turisticos: %d", estado, codigo, cidade, populacao, area, pib, turisticos );
 
 return 0;
 } 
